769faa9a8b79413169945a2b275ac6f2b6f31caf
Andrei-Flavius Ivan <afivan@mindgaze.tech>
2021-12-24 12:13:32 +0100

    Initial commit for blog post

---
+++ app-raspi/readme.txt
@@ -0,0 +1,4 @@
+Source code (Golang) here https://github.com/aler9/rtsp-simple-server
+get the Goland 1.17 version 
+SET GOOS=linux
+SET GOARCH=armv5
---
+++ app-server/setup-pm2.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/pwsh
+
+if ($args.length -le 0) {
+	Write-Error "You need to supply an IP address!"
+
+	exit 123;
+}
+
+$ip = $args[0]
+$location = "pm2"
+$split = $(pwd).Path.Split('/')
+$app = $split[$split.Length - 1]
+
+ssh -i ./ssh-keys/id_rsa root@$ip "mkdir -p $location/$app"
+scp -i ./ssh-keys/id_rsa -r rtsp-simple-server *.json *.yml  *.sh "root@${ip}:~/$location/$app"
+ssh -i ./ssh-keys/id_rsa root@$ip "cd $location/$app; pm2 start rtsp-simple-server -n $app && pm2 startup && pm2 save"
